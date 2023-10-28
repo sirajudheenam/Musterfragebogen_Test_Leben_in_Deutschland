af8d004b973edab9bb33a58590bda3720dcf4aa8
Sirajudheen Mohamed Ali <sirajudheenam@gmail.com>
2023-10-28 16:45:36 +0200

    add gtag analytics

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -6,7 +6,9 @@ function StartScreen() {
   return (
     <div className="start">
       <h2>Welcome to Life in Deutschland Quiz!</h2>
-      <h3>{numQuestions} questions to test your React mastery</h3>
+      <h3>
+        {numQuestions} questions to test your knowledge on life in Germany.
+      </h3>
       <button
         className="btn btn-ui"
         onClick={() => dispatch({ type: 'start' })}
